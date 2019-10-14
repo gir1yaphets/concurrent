a3af9a431431f3b94b4bbaa165558531bd609dcf
pengxiaolve <763791240@qq.com>
2019-10-14 10:15:42 -0700

    Add ex2 and ex3

--- src/ex2/ex22/CarWorld.java
+++ src/ex2/ex22/CarWorld.java
@@ -22,9 +22,9 @@ class CarWorld extends JPanel {
     ArrayList<Car> redCars = new ArrayList<Car>();
 
     public CarWorld() {
-	controller = new TrafficController();
+        controller = new TrafficController();
         MediaTracker mt = new MediaTracker(this);
-	Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
 
         redCar = toolkit.getImage("image/redcar.gif");
         mt.addImage(redCar, 0);
@@ -41,32 +41,32 @@ class CarWorld extends JPanel {
             System.out.println("Couldn't load one of the images");
         }
 
-	redCars.add(new Car(Car.REDCAR,null,redCar,null));
-	blueCars.add(new Car(Car.BLUECAR,null,blueCar,null));
-        setPreferredSize(new Dimension(bridge.getWidth(null),bridge.getHeight(null)));
+        redCars.add(new Car(Car.REDCAR, null, redCar, null));
+        blueCars.add(new Car(Car.BLUECAR, null, blueCar, null));
+        setPreferredSize(new Dimension(bridge.getWidth(null), bridge.getHeight(null)));
     }
 
 
     public void paintComponent(Graphics g) {
-	g.drawImage(bridge,0,0,this);
+        g.drawImage(bridge, 0, 0, this);
         for (Car c : redCars) c.draw(g);
         for (Car c : blueCars) c.draw(g);
     }
 
     public void addCar(final int cartype) {
-	SwingUtilities.invokeLater(new Runnable () {
-		public void run() {
-		    Car c;
-		    if (cartype==Car.REDCAR) {
-			c = new Car(cartype,redCars.get(redCars.size()-1),redCar,controller);
-			redCars.add(c);
-		    } else {
-			c = new Car(cartype,blueCars.get(blueCars.size()-1),blueCar,controller);
-			blueCars.add(c);
-		    }
-		    new Thread(c).start();
-	        }
-	    });
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Car c;
+                if (cartype == Car.REDCAR) {
+                    c = new Car(cartype, redCars.get(redCars.size() - 1), redCar, controller);
+                    redCars.add(c);
+                } else {
+                    c = new Car(cartype, blueCars.get(blueCars.size() - 1), blueCar, controller);
+                    blueCars.add(c);
+                }
+                new Thread(c).start();
+            }
+        });
     }
 
 }
